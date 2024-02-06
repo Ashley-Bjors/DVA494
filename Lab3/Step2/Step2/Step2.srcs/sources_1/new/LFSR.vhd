75939a084413315531e5e1560bebb61185f31816
CoffeElliot <elliot.b00din@gmail.com>
2024-02-06 13:10:56 +0100

    step 2 done up to 6bit

--- Lab3/Step2/Step2/Step2.srcs/sources_1/new/LFSR.vhd
+++ Lab3/Step2/Step2/Step2.srcs/sources_1/new/LFSR.vhd
@@ -1,63 +1,27 @@
+
+
+-------------------------------------- D flip flop ------------------------
 library ieee;
 use ieee.std_logic_1164.all;
-entity flipflop is
+entity DFF is
     port(
-        d,clk: in std_logic;
-        q: out std_logic
+        CLK: in std_logic;
+        D: in std_logic;
+        Q: out std_logic
     );
 end;
-architecture arch_flipflop of flipflop is
+
+architecture Arch_DFF of DFF is
 begin
-    process(clk)
-    begin
-    if clk = '1' then
-        q <= d;
-    end if;
-      
-  end process;
-end architecture;
+    process (CLK) is
+        begin
+        if (CLK'event and (CLK = '1')) then
+            Q <= D;
+        end if;
+    end process;
+end Arch_DFF;
 
---------------------------------------- flip flop done -----------------------------------------
-
-library ieee;
-use ieee.std_logic_1164.all;
-entity Reg is
-    generic(n: natural);
-    port(
-    d:in std_logic;
-    clk:in std_logic ;
-    q: out std_logic_vector (n-1 downto 0)
-    );
-end entity;
-  
- architecture arch_Reg of Reg is
- component flipflop is
-        port(
-        d,clk: in std_logic;
-        q: out std_logic
-);  
-end component;
- signal val : std_logic_vector (n-1 downto 0) ;
-begin 
-
-process(clk)
-begin
- val(0)<= d;
- for i in 1 to n-1 loop
-    val(i) <= val(i-1);
- end loop;
-    
-end process;
-
-    props: for i in n - 1 downto 0 generate 
-          inst_i :flipflop port map(val(i),clk,q(i));
-    end generate ;
-
-end architecture;
-
-
---------------------------------------- shift register done -----------------------------------------
-
+-------------------------------------- multiplexer ------------------------
 --Mulitplexer
 library ieee;
 use ieee.std_logic_1164.all;
@@ -82,49 +46,53 @@ begin
    
 end; 
 
------------------------------------- multiplexer done ------------------------
+------------------------------------------- LFSR -------------------------------------------
+
 
 library ieee;
 use ieee.std_logic_1164.all;
 entity LFSR is
-    generic (Registersize: natural);
+    generic (Registersize: natural:=2);
     port(
         Seed_in: in std_logic;
         Seed_en: in std_logic;
         CLK: in std_logic;
-        data_out: inout std_logic_vector(Registersize-1 downto 0)
+        Q_Vector: inout std_logic_vector(Registersize-1 downto 0)
     );
 end;
 
-architecture arch_LFSR of LFSR is
-
-component Reg is 
-  generic(n: natural);
-    port(
-    d:in std_logic;
-    clk:in std_logic ;
-    q: out std_logic_vector (n-1 downto 0)
-    );
-end component ;
-
-component Mux2To1 is
-    port
-    (
-        i0      :in std_logic;
-        i1      :in std_logic;
-        s0      :in std_logic;
-        out0    :out std_logic
-    );
-end component ;
-signal temp0,d_in: std_logic;
+architecture Arch_LFSR of LFSR is
+    component ShiftReg is
+        generic (NrOfBits: natural:=2);
+        port(
+            CLK: in std_logic;
+            Bit_Input: in std_logic;
+            Q_vector: inout std_logic_vector(NrOfBits-1 downto 0)
+        );  
+    end component;
+    component Mux2To1 is
+        port
+        (
+            i0:in std_logic;
+            i1:in std_logic;
+            s0:in std_logic;
+            out0:out std_logic
+        );
+    end component;
+    signal PostXor:std_logic;
+    signal Bit_Input:std_logic;
 begin
+    Mux : Mux2To1 port map(PostXor,Seed_in,Seed_en,Bit_Input);
+    ShiftReg_0 : ShiftReg generic map(Registersize) port map(CLK,Bit_Input,Q_Vector);
+    process (CLK) is
+        begin
+        if (CLK'event and (CLK = '1')) then
+            PostXor <= Q_Vector(0) xor Q_Vector(1);
+        end if;
+    end process;
+end Arch_LFSR;
 
-temp0 <= data_out(Registersize-2) xor data_out(Registersize-1);
-
-mux: Mux2To1 port map(Seed_in,temp0,Seed_en,d_in);
-inst_i : Reg generic map(Registersize) port map(d_in,CLK,data_out);
-
-end architecture;
+--------------------------------------------- tb -------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -137,73 +105,39 @@ component LFSR is
         Seed_in: in std_logic;
         Seed_en: in std_logic;
         CLK: in std_logic;
-        data_out: inout std_logic_vector(Registersize-1 downto 0)
+        Q_Vector: inout std_logic_vector(Registersize-1 downto 0)
     );
 end component;
 
-signal Seed_in,Seed_en : std_logic;
-signal CLK : std_logic := '0';
-signal data_out : std_logic_vector(4-1 downto 0); -- 4 bit
+signal Seed_in,Seed_en,CLK : std_logic := '0';
+signal data_out : std_logic_vector(5 downto 0); -- 8 bit
 
 begin 
- test : LFSR generic map(4) port map(Seed_in,Seed_en,CLK,data_out);
- 
- process 
- begin
- --clk <= not clk;
- --wait for 2ps;
+ inst_0 : LFSR generic map (6) port map(Seed_in,Seed_en,CLK,data_out);
 
- Seed_in <= '1';
- wait for 5ps;
  
+  process is
+ begin
+ clk <= not clk;
+ wait for 1ps;
+ end process;
+ 
+ process is
+ begin
+ Seed_en <= '1';
+ wait for 2ps;
+ 
+ Seed_in <= '1';
+ wait for 2ps;
+
+ Seed_en <= '0';
+ wait for 2ps;
+
  Seed_in <= '0';
- wait for 5ps;
+ wait for 2ps;
  
  end process;
  
  
 end architecture;
 
-
---library ieee;
---use ieee.std_logic_1164.all;
---entity LFSR is
---    generic (Registersize: natural:=2);
---    port(
---        Seed_in: in std_logic;
---        Seed_en: in std_logic;
---        CLK: in std_logic;
---        Q_Vector: inout std_logic_vector(Registersize-1 downto 0)
---    );
---end;
-
---architecture Arch_LFSR of LFSR is
---    component ShiftReg is
---        generic (NrOfBits: natural:=2);
---        port(
---            CLK: in std_logic;
---            Bit_Input: in std_logic;
---            Q_vector: inout std_logic_vector(NrOfBits-1 downto 0)
---        );  
---    end component;
---    component Mux2To1 is
---        port
---        (
---            i0:in std_logic;
---            i1:in std_logic;
---            s0:in std_logic;
---            o:out std_logic
---        );
---    end component;
---    signal PostXor:std_logic;
---    signal Bit_Input:std_logic;
---begin
---    Mux : Mux2To1 port map(PostXor,Seed_in,Seed_en,Bit_Input);
---    ShiftReg_0 : ShiftReg generic map(Registersize) port map(CLK,Bit_Input,Q_Vector);
---    process (CLK) is
---        begin
---        if (CLK'event and (CLK = '1')) then
---            PostXor <= Q_Vector(0) xor Q_Vector(1);
---        end if;
---    end process;
---end Arch_LFSR;
