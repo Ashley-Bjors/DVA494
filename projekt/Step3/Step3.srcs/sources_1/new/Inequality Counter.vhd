6ee63d43a6318bdcbd4df46ba61642382d204a4a
DanRyten <daniel.rytenberg@gmail.com>
2024-02-20 11:30:42 +0100

    Projekt Step 3 done and tested

--- projekt/Step3/Step3.srcs/sources_1/new/Inequality Counter.vhd
+++ projekt/Step3/Step3.srcs/sources_1/new/Inequality Counter.vhd
@@ -62,16 +62,17 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity InequalityCounter is
-    generic(n: natural := 4)
+    generic(n: natural := 4);
   port (
     in1,in2: in std_logic_vector (n-1 downto 0);
     CLK,reset : in std_logic := '0';
-    o: out integer;
+    o: out integer
   ) ;
 end InequalityCounter;
 
 architecture arch_InequalityCounter of InequalityCounter is
   component Comparator is
+    generic(Size: natural := 4);
     port(
       cmp_in1,cmp_in2: in std_logic_vector(Size-1 downto 0);
       cmp_out : out std_logic 
@@ -79,15 +80,8 @@ architecture arch_InequalityCounter of InequalityCounter is
   end component;
   component counter is
     port(
-      CLK,reset,en : in std_logic := '0';
+      CLK,reset,en : in std_logic := '0'; 
       counter_out: out integer 
-    );
-  end component;
-  component InequalityCounter is
-    port (
-      in1,in2: in std_logic_vector (n-1 downto 0);
-      CLK,reset : in std_logic := '0';
-      o: out std_logic;
     );
   end component;
 
@@ -99,3 +93,61 @@ Comparator_0 : Comparator port map(in1,in2,post_cmp);
 counter_0 : counter port map(CLK,reset,post_cmp,o);
 
 end architecture ; -- arch
+
+library ieee;            
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb is end;
+
+architecture arch_tb of tb is
+    component InequalityCounter is
+    generic(n: natural := 4);
+  port (
+    in1,in2: in std_logic_vector (n-1 downto 0);
+    CLK,reset : in std_logic := '0';
+    o : out integer
+  ) ;
+  end component;
+  
+  signal in1, in2 : std_logic_vector (3 downto 0);
+  signal clk, reset : std_logic := '0';
+  signal o : integer;
+
+begin 
+ inCounter0 : InequalityCounter generic map(4) port map(in1, in2, clk, reset, o);
+ 
+  process is
+  begin
+  clk <= not clk;
+  wait for 1ps;
+  end process;
+ 
+ process is 
+ begin
+ 
+ reset <= '0';
+ 
+ in1 <= "1111";
+ in2 <= "1110";
+ wait for 2ps;
+  
+ in1 <= "1101";
+ in2 <= "1100";
+ wait for 2ps;
+ 
+ in1 <= "1000";
+ in2 <= "0000";
+ wait for 2ps;
+  
+ in1 <= "1111";
+ in2 <= "1111";
+ wait for 2ps;
+ 
+ reset <= '1';
+ 
+ wait for 2ps;
+ 
+ 
+ end process;
+ end architecture;
